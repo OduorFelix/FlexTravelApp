5acc09f139bc6801961a93b23a5004a1f2c0c509
Felix_Oduor <oduorfelix2@gmail.com>
2021-01-31 13:16:01 +0300

    Initial Commit

---
+++ components/Liked.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const Liked = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Liked</Text>
+    </View>
+  );
+};
+
+export default Liked;
